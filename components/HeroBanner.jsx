8a236ca7cdc2117923c221fd3503c927a4b7eba5
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-05-27 21:24:05 -0300

    feat: Hero banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,22 +1,26 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { urlFor } from '../lib/client';
 
 function HeroBanner({ heroBanner }) {
+  const bannerUrl = urlFor(heroBanner.image).url();
   return (
     <div className="hero-banner-container">
       <div>
         <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt="Headphones" className="hero-banner-image" />
+        <div className="hero-banner-image">
+          <Image src={bannerUrl} alt="Headphones" layout="fill" />
+        </div>
 
         <div>
           <Link href={`/products/${heroBanner.product}`}>
             <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
-            <h5>Description</h5>
+            <h5>Descrição</h5>
             <p>{heroBanner.desc}</p>
           </div>
         </div>
