dc85b221b1121bfa5f8b171fb289478d5ae58da8
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-05-27 21:24:13 -0300

    feat: Footer banner

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,8 +1,38 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import { urlFor } from '../lib/client';
 
-function FooterBanner() {
+function FooterBanner({ footerBanner }) {
   return (
-    <div>FooterBanner</div>
+    <div className="footer-banner-container">
+      <div className="banner-desc">
+        <div className="left">
+          <p>{footerBanner.discount}</p>
+          <h3>{footerBanner.largeText1}</h3>
+          <h3>{footerBanner.largeText2}</h3>
+          <p>{footerBanner.saleTime}</p>
+        </div>
+        <div className="right">
+          <p>{footerBanner.smallText}</p>
+          <h3>{footerBanner.midText}</h3>
+          <p>{footerBanner.desc}</p>
+          <Link href={`/product/${footerBanner.product}`}>
+            <button type="button">{footerBanner.buttonText}</button>
+          </Link>
+        </div>
+
+        <div className="footer-banner-image">
+          <Image
+            src={urlFor(footerBanner.image).url()}
+            width={400}
+            height={400}
+          />
+
+        </div>
+
+      </div>
+    </div>
   );
 }
 
