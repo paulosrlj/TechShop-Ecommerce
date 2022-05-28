2286dd2197845bbf337c4085a04f96eb7d34b005
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-05-27 21:24:41 -0300

    feat: page layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import Head from 'next/head';
 
-function Layout() {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+function Layout({ children }) {
   return (
-    <div>Layout</div>
+    <div className="layout">
+      <Head>
+        <title>Eletronics Produtos</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <main className="main-container">
+          {children}
+        </main>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   );
 }
 
